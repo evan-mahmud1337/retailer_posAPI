815fa9190b1af53d93aaa86b564b78578e52503e
abdullahalfahad98 <64860948+abdullahalfahad98@users.noreply.github.com>
2024-04-01 14:49:04 +0600

    [Add] Total amount (monthly, weekly, yesterday & total amount)

--- sales/api/views.py
+++ sales/api/views.py
@@ -61,16 +61,45 @@ class SaleRetrieveView(APIView):
 class DashboardView(APIView):
     
     def get(self, request, format=None):
-        yesterday=datetime.now().date()-timedelta(days=1)
+        today = datetime.now().date()
+        yesterday= today - timedelta(days=1)
         print(yesterday)
-        week=datetime.now().date()-timedelta(weeks=1)
-        months=datetime.now().date()-timedelta(days=30)
+        week= today - timedelta(weeks=7)
+        months= today - timedelta(days=30)
 
         total_sale=Sale.objects.count()
         sale_yesterday=Sale.objects.filter(created_date=yesterday).count()
         sale_week=Sale.objects.filter(created_date__gte=week).count()
         sale_month=Sale.objects.filter(created_date__gte=months).count()
+
+         # Retrieving total sale amounts for different time intervals
+        sale_yesterday_amount = Sale.objects.filter(created_date=yesterday).aggregate(total_amount=Sum('total'))['total_amount']
+        sale_week_amount = Sale.objects.filter(created_date__gte=week_start).aggregate(total_amount=Sum('total'))['total_amount']
+        sale_month_amount = Sale.objects.filter(created_date__gte=month_start).aggregate(total_amount=Sum('total'))['total_amount']
+        total_sale_amount = Sale.objects.aggregate(total_amount=Sum('total'))['total_amount']
+
+         # If there are no sales for a particular period, set the total amount to 0
+        sale_yesterday_amount = sale_yesterday_amount if sale_yesterday_amount else 0
+        sale_week_amount = sale_week_amount if sale_week_amount else 0
+        sale_month_amount = sale_month_amount if sale_month_amount else 0
+        total_sale_amount = total_sale_amount if total_sale_amount else 0
+
+        # Constructing response data
+        response_data = {
+            'total_sale': {
+                'sale_month':sale_month
+                'sale_week':sale_week,
+                'sale_yesterday':sale_yesterday
+                'total_sale':total_sale,
+            },
+            'total_sale_amount': {
+                'monthly': sale_month_amount,
+                'weekly': sale_week_amount,
+                'yesterday': sale_yesterday_amount,
+                'total': total_sale_amount
+            }
+        }
         
-        context={'total_sale':total_sale,'sale_yesterday':sale_yesterday,'sale_week':sale_week,
-                 'sale_month':sale_month}
+        # context={'total_sale':total_sale,'sale_yesterday':sale_yesterday,'sale_week':sale_week,
+        #          'sale_month':sale_month}
         return Response(context, status=status.HTTP_200_OK)
